d5f36e93e2661a83ca189a399e5333d4c57e73a8
k452 <docomo9746@gmail.com>
2021-01-20 11:40:16 +0900

    2進数の生成を軽くする

--- main.go
+++ main.go
@@ -2,38 +2,123 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"time"
 )
 
 func main() {
 	start := time.Now()
-	for pos := 0; pos < 6; pos++ {
-		for j := 0b0; j <= 0b11111; j++ { //5階差分
-			//差分ベクトルにcを差し込む処理
-			t := (j >> pos) & create2(5-pos)
-			b := j & create2(pos)
-			//rand.Seed(time.Now().UnixNano())
-			in := 0b1
-			output := (((t << 1) | in) << pos) | b
-			fmt.Printf("%06b\n", output)
-		}
-		fmt.Println("--------------------")
-	}
-	//for i := 0; i < 8; i++ {
-	//	fmt.Printf("%b\n", (a>>(7-i))&1)
-	//}
-
+	pos := 2
+	txt := 0b10101
+	t := (txt >> pos) & create2(5-pos)
+	b := txt & create2(pos)
+	fmt.Printf("%0b\n", t)
+	fmt.Printf("%0b\n", b)
 	fmt.Println("\n実行時間：", time.Since(start))
 }
 
 func create2(num int) int {
-	txt := ""
-	for j := 0; j < num; j++ {
-		txt += "1"
+	res := 0b0
+	switch num {
+	case 0:
+		res = 0b0
+		break
+	case 1:
+		res = 0b1
+		break
+	case 2:
+		res = 0b11
+		break
+	case 3:
+		res = 0b111
+		break
+	case 4:
+		res = 0b1111
+		break
+	case 5:
+		res = 0b11111
+		break
+	case 6:
+		res = 0b111111
+		break
+	case 7:
+		res = 0b1111111
+		break
+	case 8:
+		res = 0b11111111
+		break
+	case 9:
+		res = 0b111111111
+		break
+	case 10:
+		res = 0b1111111111
+		break
+	case 11:
+		res = 0b11111111111
+		break
+	case 12:
+		res = 0b111111111111
+		break
+	case 13:
+		res = 0b1111111111111
+		break
+	case 14:
+		res = 0b11111111111111
+		break
+	case 15:
+		res = 0b111111111111111
+		break
+	case 16:
+		res = 0b1111111111111111
+		break
+	case 17:
+		res = 0b11111111111111111
+		break
+	case 18:
+		res = 0b111111111111111111
+		break
+	case 19:
+		res = 0b1111111111111111111
+		break
+	case 20:
+		res = 0b11111111111111111111
+		break
+	case 21:
+		res = 0b111111111111111111111
+		break
+	case 22:
+		res = 0b1111111111111111111111
+		break
+	case 23:
+		res = 0b11111111111111111111111
+		break
+	case 24:
+		res = 0b111111111111111111111111
+		break
+	case 25:
+		res = 0b1111111111111111111111111
+		break
+	case 26:
+		res = 0b11111111111111111111111111
+		break
+	case 27:
+		res = 0b111111111111111111111111111
+		break
+	case 28:
+		res = 0b1111111111111111111111111111
+		break
+	case 29:
+		res = 0b11111111111111111111111111111
+		break
+	case 30:
+		res = 0b111111111111111111111111111111
+		break
+	case 31:
+		res = 0b1111111111111111111111111111111
+		break
+	default:
+		panic("範囲外")
 	}
-	res, _ := strconv.ParseInt(txt, 2, 32)
-	return int(res)
+	return res
 }
 
 func nSplit(msg string, splitlen int) []string {
